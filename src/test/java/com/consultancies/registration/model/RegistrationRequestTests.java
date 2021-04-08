0e7dc9221a7d1f889b436fb0468e5f792bfd6f84
andimon <andrevella9@gmail.com>
2021-04-08 11:12:03 +0200

    some more tests

--- src/test/java/com/consultancies/registration/model/RegistrationRequestTests.java
+++ src/test/java/com/consultancies/registration/model/RegistrationRequestTests.java
@@ -1,19 +1,57 @@
 package com.consultancies.registration.model;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class RegistrationRequestTests {
+    RegistrationRequest registrationRequest;
+    int age;
+    String fullName;
+    @BeforeEach
+    public  void setup() {
+        registrationRequest = new RegistrationRequest("Andre Vella",18);
+        age=0;
+        fullName=" ";
 
+    }
+    @AfterEach
+    public void teardown() {
+        registrationRequest = null;
+
+
+    }
     @Test
     public void testRequestCreation() {
-        RegistrationRequest registrationRequest = new RegistrationRequest("Andre Vella",18);
+
         String fullName = registrationRequest.getFullName();
-        int age = registrationRequest.getAge();
+        age = registrationRequest.getAge();
         Assertions.assertEquals("Andre Vella",fullName);
         Assertions.assertEquals(18,age);
 
     }
+    @Test
+    public void testRequestSetAgeMethod(){
+        registrationRequest.setAge(20);
+        age = registrationRequest.getAge();
+        Assertions.assertEquals(20,age);
+
+
+
+
+    }
+    @Test
+    public void testRequestSetFullNameMethod(){
+        registrationRequest.setFullName("Sergio P.");
+        registrationRequest.setFullName("Kelsey M.");
+
+        fullName = registrationRequest.getFullName();
+        Assertions.assertEquals("Kelsey M.",fullName);
+
+
+
+
+    }
+
+
 
 
 

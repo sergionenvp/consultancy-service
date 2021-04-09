4c03aaafc08c1ccda4951293ab87a52522d7951b
andimon <andrevella9@gmail.com>
2021-04-09 08:36:42 +0200

    update

---
+++ src/main/java/com/consultancies/registration/model/RegistrationResponse.java
@@ -0,0 +1,15 @@
+package com.consultancies.registration.model;
+
+public class RegistrationResponse {
+    private String fullName;
+    private int age;
+    public RegistrationResponse(String fullName,int age){
+        this.fullName=fullName;
+        this.age=age;
+
+    }
+    public String getFullName(){return fullName;}
+    public void setFullName(String  fullName){this.fullName=fullName;}
+    public int getAge(){return  age;}
+    public void setAge(int age){this.age=age;}
+}
